e586537df8e3770470cfd7a1d1ec4574601d42d7
daffarafi <daffarafip24@gmail.com>
2023-02-27 11:37:49 +0700

    responsive homepage

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -19,20 +19,20 @@ export const Navbar: React.FC = () => {
                 <li>
                     <Link
                         href={'/'}
-                        className="flex gap-4 hover:bg-secondary/25 transition p-3 pr-6 rounded-full"
+                        className="flex gap-4 hover:bg-secondary/25 transition w-fit xl:w-auto p-3 xl:pr-6 rounded-full"
                     >
                         <Home />
-                        <p>Beranda</p>
+                        <p className="hidden xl:block">Beranda</p>
                     </Link>
                 </li>
                 {user ? (
-                    <li>
+                    <li className="">
                         <Link
                             href={`/users/${user.username}`}
-                            className="flex gap-4 hover:bg-secondary/25 transition p-3 pr-6 rounded-full"
+                            className="flex gap-4 hover:bg-secondary/25 transition w-fit xl:w-auto p-3 xl:pr-6 rounded-full"
                         >
                             <Profile />
-                            <p>Profile</p>
+                            <p className="hidden xl:block">Profile</p>
                         </Link>
                     </li>
                 ) : (
@@ -41,10 +41,10 @@ export const Navbar: React.FC = () => {
                 <li>
                     <Link
                         href={'/explore'}
-                        className="flex gap-4 hover:bg-secondary/25 transition p-3 pr-6 rounded-full"
+                        className="flex gap-4 hover:bg-secondary/25 transition w-fit xl:w-auto p-3 xl:pr-6 rounded-full"
                     >
                         <Compass />
-                        <p>Jelajahi</p>
+                        <p className="hidden xl:block">Jelajahi</p>
                     </Link>
                 </li>
             </>
@@ -52,18 +52,18 @@ export const Navbar: React.FC = () => {
     }
 
     return (
-        <nav>
-            <div className="flex flex-col pr-4 gap-5 pt-3  sticky top-0">
-                <div className="flex text-xl  gap-4">
+        <nav className="fixed bottom-0 w-full sm:w-auto sm:static">
+            <div className="flex sm:flex-col pr-4 gap-5  sticky top-0">
+                <div className="hidden sm:flex text-xl gap-4">
                     <Link
                         href={'/'}
-                        className="flex gap-4 hover:bg-secondary/25 transition p-3 pr-6 rounded-full"
+                        className="flex gap-4 hover:bg-secondary/25 transition w-fit xl:w-auto p-3 xl:pr-6 rounded-full"
                     >
                         <div className="w-[25px] h-[25px] bg-gray-500 rounded-full"></div>
-                        <p className="">TechFess</p>
+                        <p className="xl:block hidden">TechFess</p>
                     </Link>
                 </div>
-                <ul className="flex flex-col gap-3 text-xl">
+                <ul className="flex justify-between w-full sm:w-auto sm:justify-start sm:flex-col gap-3 text-xl">
                     {renderNavbar()}
                 </ul>
             </div>
